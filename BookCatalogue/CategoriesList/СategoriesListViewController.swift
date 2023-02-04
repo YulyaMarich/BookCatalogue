50b3c472788181cfc0debf82699d2f6db158ff8a
Julia <>
2023-02-04 17:51:13 +0200

    Changed Collection views

--- BookCatalogue/CategoriesList/СategoriesListViewController.swift
+++ BookCatalogue/CategoriesList/СategoriesListViewController.swift
@@ -7,12 +7,20 @@
 
 import UIKit
 
-class СategoriesListViewController: UICollectionViewController {
+class СategoriesListViewController: UIViewController {
+    
+    private lazy var collectionView: UICollectionView = {
+        let layout = UICollectionViewFlowLayout()
+        layout.scrollDirection = .vertical
+        let collectionView = UICollectionView(frame: .zero,
+                                              collectionViewLayout: layout)
+        return collectionView
+    }()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-       setUpNavigationController()
-       setUpCollectionView()
+        setUpNavigationController()
+        setUpCollectionView()
     }
     
     private func setUpNavigationController() {
@@ -22,47 +30,52 @@ class СategoriesListViewController: UICollectionViewController {
     }
     
     private func setUpCollectionView() {
-        collectionView.backgroundColor = UIColor(red: 0.973, green: 0.957, blue: 0.930, alpha: 1)
-        collectionView.register(CategoryCollectionViewCell.self, forCellWithReuseIdentifier: "Cell")
+        view.addSubview(collectionView)
         
-        let layout = collectionView.collectionViewLayout as! UICollectionViewFlowLayout
-        let itemsAtRow: CGFloat = 2
+        collectionView.translatesAutoresizingMaskIntoConstraints = false
+        collectionView.leftAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leftAnchor).isActive = true
+        collectionView.rightAnchor.constraint(equalTo: view.safeAreaLayoutGuide.rightAnchor).isActive = true
+        collectionView.bottomAnchor.constraint(equalTo: view.bottomAnchor).isActive = true
+        collectionView.topAnchor.constraint(equalTo: view.topAnchor).isActive = true
+        
+        collectionView.dataSource = self
+        collectionView.delegate = self
+        
+        collectionView.backgroundColor = UIColor(red: 0.973, green: 0.957, blue: 0.930, alpha: 1)
+        collectionView.register(CategoryCollectionViewCell.self, forCellWithReuseIdentifier: "CategoryCell")
+        
         let sectionInserts = UIEdgeInsets(top: 0, left: 15, bottom: 0, right: 15)
-        let minimumLineSpacing: CGFloat = 10
+        let itemsAtRow: CGFloat = 2
         let minimumInteritemSpacing: CGFloat = 10
-        let screenWidth = collectionView.frame.width
+        let screenWidth = view.frame.width
         let spacing = minimumInteritemSpacing * (itemsAtRow - 1)
         let availableWidth = screenWidth - sectionInserts.left - sectionInserts.right - spacing
         let itemWidth = availableWidth / itemsAtRow
-        
-        layout.itemSize = CGSize(width: CGFloat(itemWidth), height: CGFloat(itemWidth + 10))
-        layout.sectionInset = sectionInserts
-        layout.minimumLineSpacing = minimumLineSpacing
+    
+        let layout = collectionView.collectionViewLayout as! UICollectionViewFlowLayout
         layout.minimumInteritemSpacing = minimumInteritemSpacing
+        layout.minimumLineSpacing = 10
+        layout.itemSize = CGSize(width: itemWidth, height: itemWidth + 10)
+        layout.sectionInset = sectionInserts
+    }
+}
+
+extension СategoriesListViewController: UICollectionViewDataSource, UICollectionViewDelegate, UICollectionViewDelegateFlowLayout {
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 10
     }
     
-    override func numberOfSections(in collectionView: UICollectionView) -> Int {
-        return 1
-    }
-    
-    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return 53
-    }
-    
-    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "Cell", for: indexPath) as! CategoryCollectionViewCell
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "CategoryCell",
+                                                      for: indexPath) as! CategoryCollectionViewCell
         cell.setUpContentView()
-        
         return cell
     }
     
-    override func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
-        let layout = UICollectionViewFlowLayout()
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
         let viewModel = BooksListViewModel()
         let booksListVC = BooksListViewController(viewModel: viewModel)
-        
-        booksListVC.collectionView = UICollectionView(frame: .zero, collectionViewLayout: layout)
-        
         navigationController?.pushViewController(booksListVC, animated: true)
     }
 }
--- BookCatalogue/SupportingFiles/SceneDelegate.swift
+++ BookCatalogue/SupportingFiles/SceneDelegate.swift
@@ -15,10 +15,9 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         guard let _ = (scene as? UIWindowScene) else { return }
         guard let windowScene = (scene as? UIWindowScene) else { return }
-        let layout = UICollectionViewFlowLayout()
         window = UIWindow(frame: UIScreen.main.bounds)
         window?.windowScene = windowScene
-        window?.rootViewController = UINavigationController(rootViewController:СategoriesListViewController(collectionViewLayout: layout))
+        window?.rootViewController = UINavigationController(rootViewController:СategoriesListViewController())
         window?.makeKeyAndVisible()
     }
 
