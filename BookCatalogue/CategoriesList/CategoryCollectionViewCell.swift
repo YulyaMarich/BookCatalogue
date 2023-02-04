7ee117888054ac6172bfc133e7573baa22b97783
YulyaMarich <89585518+YulyaMarich@users.noreply.github.com>
2023-02-04 14:39:16 +0200

    Set up BookCatalogue controller and configured appearance of its cell (#3)
    Co-authored-by: Julia <>

---
+++ BookCatalogue/CategoriesList/CategoryCollectionViewCell.swift
@@ -0,0 +1,94 @@
+//
+//  CategoryCollectionViewCell.swift
+//  BookCatalogue
+//
+//  Created by Julia on 03.02.2023.
+//
+
+import UIKit
+
+class CategoryCollectionViewCell: UICollectionViewCell {
+    private var categoryNameLabel: UILabel = {
+        let categoryNameLabel = UILabel()
+        return categoryNameLabel
+    }()
+    
+    private var categoryInfoStack: UIStackView = {
+        let categoryInfoStack = UIStackView()
+        return categoryInfoStack
+    }()
+    
+    private var updateFrequencyLabel: UILabel = {
+        let updateFrequencyLabel = UILabel()
+        return updateFrequencyLabel
+    }()
+    
+    private var newestPublishedDate: UILabel = {
+        let newestPublishedDate = UILabel()
+        return newestPublishedDate
+    }()
+    
+    private var divider: UIView = {
+        let divider = UIView()
+        return divider
+    }()
+    
+    func setUpContentView() {
+        contentView.backgroundColor = .white
+        contentView.layer.cornerRadius = 4
+        addSubviews()
+        setUpConstraints()
+        setUpCategoryInfoStack()
+        makeShadow()
+        divider.backgroundColor = UIColor(red: 0.353, green: 0.294, blue: 0.267, alpha: 1)
+        categoryNameLabel.text = "Combined Print and E-Book Nonfiction"
+        categoryNameLabel.font = UIFont.systemFont(ofSize: 27, weight: .heavy)
+        categoryNameLabel.lineBreakMode = .byWordWrapping
+        categoryNameLabel.numberOfLines = 0
+        updateFrequencyLabel.text = "Updated: weekly"
+        newestPublishedDate.text = "Last Published: 22.12.2022"
+    }
+    
+    private func addSubviews() {
+        contentView.addSubview(categoryNameLabel)
+        contentView.addSubview(categoryInfoStack)
+        contentView.addSubview(divider)
+        categoryInfoStack.addArrangedSubview(updateFrequencyLabel)
+        categoryInfoStack.addArrangedSubview(newestPublishedDate)
+    }
+    
+    private func setUpConstraints() {
+        categoryNameLabel.translatesAutoresizingMaskIntoConstraints = false
+        categoryNameLabel.topAnchor.constraint(equalTo: contentView.topAnchor).isActive = true
+        categoryNameLabel.leftAnchor.constraint(equalTo: contentView.leftAnchor, constant: 10).isActive = true
+        categoryNameLabel.rightAnchor.constraint(equalTo: contentView.rightAnchor, constant: -10).isActive = true
+        categoryNameLabel.bottomAnchor.constraint(greaterThanOrEqualTo: divider.topAnchor, constant: -3).isActive = true
+        categoryNameLabel.setContentHuggingPriority(UILayoutPriority(rawValue: 249), for: .vertical)
+        categoryNameLabel.setContentCompressionResistancePriority(UILayoutPriority(749), for: .vertical)
+        
+        divider.translatesAutoresizingMaskIntoConstraints = false
+        divider.leftAnchor.constraint(equalTo: contentView.leftAnchor, constant: 5).isActive = true
+        divider.rightAnchor.constraint(equalTo: contentView.rightAnchor, constant: -5).isActive = true
+        divider.bottomAnchor.constraint(equalTo: categoryInfoStack.topAnchor, constant: -3).isActive = true
+        divider.heightAnchor.constraint(equalToConstant: 1.5).isActive = true
+        
+        categoryInfoStack.translatesAutoresizingMaskIntoConstraints = false
+        categoryInfoStack.leftAnchor.constraint(equalTo: contentView.leftAnchor, constant: 10).isActive = true
+        categoryInfoStack.rightAnchor.constraint(equalTo: contentView.rightAnchor, constant: -10).isActive = true
+        categoryInfoStack.bottomAnchor.constraint(equalTo: contentView.bottomAnchor, constant: -10).isActive = true
+    }
+    
+    private func setUpCategoryInfoStack() {
+        categoryInfoStack.axis = .vertical
+        categoryInfoStack.spacing = 5
+        updateFrequencyLabel.font = UIFont.systemFont(ofSize: UIScreen.main.bounds.height < 750 ? 10 : 10, weight: .medium)
+        newestPublishedDate.font = UIFont.systemFont(ofSize: UIScreen.main.bounds.height < 750 ? 10 : 10, weight: .medium)
+    }
+    
+    private func makeShadow() {
+        self.layer.shadowOffset = CGSize(width: 0, height: 0)
+        self.layer.shadowRadius = 3
+        self.layer.shadowColor = UIColor.black.cgColor
+        self.layer.shadowOpacity = 0.3
+    }
+}
--- BookCatalogue/CategoriesList/СategoriesListViewController.swift
+++ BookCatalogue/CategoriesList/СategoriesListViewController.swift
@@ -7,9 +7,51 @@
 
 import UIKit
 
-class СategoriesListViewController: UIViewController {
-
+class СategoriesListViewController: UICollectionViewController {
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+       setUpNavigationController()
+       setUpCollectionView()
+    }
+    
+    private func setUpNavigationController() {
+        navigationItem.title = "The New York Times"
+        navigationController?.navigationBar.prefersLargeTitles = true
+    }
+    
+    private func setUpCollectionView() {
+        collectionView.backgroundColor = UIColor(red: 0.973, green: 0.957, blue: 0.930, alpha: 1)
+        collectionView.register(CategoryCollectionViewCell.self, forCellWithReuseIdentifier: "Cell")
+        
+        let layout = collectionView.collectionViewLayout as! UICollectionViewFlowLayout
+        let itemsAtRow: CGFloat = 2
+        let sectionInserts = UIEdgeInsets(top: 0, left: 15, bottom: 0, right: 15)
+        let minimumLineSpacing: CGFloat = 10
+        let minimumInteritemSpacing: CGFloat = 10
+        let screenWidth = collectionView.frame.width
+        let spacing = minimumInteritemSpacing * (itemsAtRow - 1)
+        let availableWidth = screenWidth - sectionInserts.left - sectionInserts.right - spacing
+        let itemWidth = availableWidth / itemsAtRow
+        
+        layout.itemSize = CGSize(width: CGFloat(itemWidth), height: CGFloat(itemWidth + 10))
+        layout.sectionInset = sectionInserts
+        layout.minimumLineSpacing = minimumLineSpacing
+        layout.minimumInteritemSpacing = minimumInteritemSpacing
+    }
+    
+    override func numberOfSections(in collectionView: UICollectionView) -> Int {
+        return 1
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 53
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "Cell", for: indexPath) as! CategoryCollectionViewCell
+        cell.setUpContentView()
+        
+        return cell
     }
 }
